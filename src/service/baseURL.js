5e3b1e03a1407a8ad07747dd535ca6fbafb0a988
Marcio <marcioabcd11@gmail.com>
2021-11-28 21:15:32 -0300

    change vercel production env

--- src/service/baseURL.js
+++ src/service/baseURL.js
@@ -1,9 +1,9 @@
 import process from 'process';
 
-const { NODE_ENV } = process.env;
+const { VERCEL_ENV } = process.env;
 
-const BASE_URL = (NODE_ENV === 'production')
-  ? 'https://provi-hackaton.herokuapp.com/'
+const BASE_URL = (VERCEL_ENV === 'production')
+  ? 'https://provi-hackaton.herokuapp.com'
   : 'http://localhost:4000';
 
 export default BASE_URL;
