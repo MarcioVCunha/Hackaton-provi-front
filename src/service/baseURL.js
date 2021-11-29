7048fa229dbc924299702fccc75f1da4f9504a67
Marcio <marcioabcd11@gmail.com>
2021-11-28 21:57:01 -0300

    fixig react error on env

--- src/service/baseURL.js
+++ src/service/baseURL.js
@@ -1,3 +1,7 @@
-const BASE_URL = 'https://provi-hackaton.herokuapp.com';
+const { REACT_APP_NODE_ENV } = process.env;
+
+const BASE_URL = (REACT_APP_NODE_ENV === 'production')
+  ? 'https://provi-hackaton.herokuapp.com'
+  : 'http://localhost:4000';
 
 export default BASE_URL;
