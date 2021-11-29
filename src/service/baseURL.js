02562895ebc475e17462d1e065a50a819dfff14c
Marcio <marcioabcd11@gmail.com>
2021-11-28 21:09:39 -0300

    vercel bug fix

--- src/service/baseURL.js
+++ src/service/baseURL.js
@@ -2,7 +2,7 @@ import process from 'process';
 
 const { NODE_ENV } = process.env;
 
-const BASE_URL = (NODE_ENV === 'production')
+const BASE_URL = (NODE_ENV === 'prod')
   ? 'https://provi-hackaton.herokuapp.com/'
   : 'http://localhost:4000';
 
